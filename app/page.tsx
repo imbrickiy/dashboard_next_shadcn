904f8e8a91c1c372b3d37e32e6e87c31d430241e
imbrickiy <imbrickiy@ya.ru>
2024-07-01 16:04:21 +0300

    added change theme on page with toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+'use client'
+import { Button } from '@/components/ui/button'
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+	const { theme, setTheme } = useTheme()
+
+	return (
+		<main className='flex min-h-screen flex-col items-center gap-4 p-24'>
+			<Button
+				variant='outline'
+				size='icon'
+				onClick={() => {
+					setTheme(theme === 'dark' ? 'light' : 'dark')
+				}}
+			>
+				<Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+				<Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+				<span className='sr-only'>Toggle theme</span>
+			</Button>
+			{/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
@@ -39,6 +55,8 @@ export default function Home() {
         />
       </div>
 
+      <Button>Click me</Button>
+      
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -107,7 +125,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
-    </main>
-  );
+      </div> */}
+		</main>
+	)
 }
